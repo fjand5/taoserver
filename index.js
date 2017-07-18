4ca6ba68d58490758c24210ac1eecf54e059bbe3
fjand5 <huynhtheluat@gmail.com>
2017-07-18 21:43:36 +0700

    first commit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ http.createServer((req,res)=>{
 	sl++;
 	console.log("So thiet bi "+ sl);
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.end"(<h1>Chao thiet bi + sl </h1>");
+	res.end("<h1>Chao thiet bi + sl </h1>");
 }).listen(80);
